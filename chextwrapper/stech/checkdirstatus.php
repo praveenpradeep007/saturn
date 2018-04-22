4c5a3bfa4fa348c692404036aa5e9862cac76f00
praveen <pradeepspraveen@gmail.com>
2018-04-22 11:20:49 +0530

    refreshes every minute . reloading of page has been replaced with ajax calls .

---
+++ chextwrapper/stech/checkdirstatus.php
@@ -0,0 +1,8 @@
+<?php
+		$dirsize = sizeof(scandir("../../allketuploads"));
+        $dirstatus = "notempty";
+        if($dirsize<7){
+            $dirstatus = "empty";
+        }
+        echo $dirstatus;
+?>
--- chextwrapper/stech/fbimgprocessor.php
+++ chextwrapper/stech/fbimgprocessor.php
@@ -3,7 +3,7 @@
         include('phpdatabases/connect-db.php');
         $phppostsrc = mysqli_real_escape_string($conn, $_POST['phppostsrcpix']);
         $phpfacthead = mysqli_real_escape_string($conn, $_POST['phpfactheader']);
-        $setpostinfo = "UPDATE fbketfacts SET facthead='$phpfacthead', facts='$phppostsrc', status='undone'";
+        $setpostinfo = "UPDATE fbketfacts SET facthead='$phpfacthead',facts='$phppostsrc',status='undone'";
         $conn->query($setpostinfo);
         $content = file_get_contents("http://localhost/chextwrapper/stech/imagecreator.php");
         $concstr = rand(100,9000);
@@ -15,34 +15,40 @@
         include('phpdatabases/close-connect-db.php');
         exit(0);
     }
-    else{
-        $dirsize = sizeof(scandir("../../allketuploads"));
-        $dirstatus = "notempty";
-        if($dirsize<4){
-            $dirstatus = "empty";
-        }
-    }
 ?>
 
 <html>
 <head>
     <title>fbimgprocessor</title>
+    <link rel="shortcut icon" type="image/x-icon" href="../chextsurf/img/chextsurf.png" />
+    <script src="../chextsurf/js/jquery-3.2.1.min.js"></script>
+    <script>
+    function ajaxcall(){    
+        $.ajax({ 
+            type:"POST",
+            url:"checkdirstatus.php",
+            data:{},
+            cache:false,
+            success:function(data){
+                if(encodeURI(data) != "%0D%0A%0D%0A"){
+                    if(data == "empty"){
+                        var allalpha = "abcdefghijklmnopqrstuvwxyz"; 
+                        var x = Math.floor((Math.random() * 25) + 0);    
+                        var openedwindow  = window.open("http://www.hobbyprojects.com/computer-terms-dictionary/computer-dictionary-"+allalpha[x]+"#chextlinkscan",
+                            "_blank","width=300,height=200");
+                        setTimeout(function(){ openedwindow.close(); }, 30000);
+                    }             
+                }
+            }
+        });
+    } 
+
+    var refinterval = 60;
+    var trimtosec = refinterval*1000;
+    setInterval(ajaxcall, trimtosec);
+    </script>
 </head>   
-<body>
+<body style="overflow-x:hidden;">
+    <img src="../chextsurf/img/chextsurf.png" style="margin:19% 45%;"/>
 </body>
-<script src="../chextsurf/js/jquery-3.2.1.min.js"></script>
-<script>   
-var refinterval = 900;
-var trimtosec = refinterval*1000;
-var dirstatus = "<?php echo $dirstatus; ?>";
-if(dirstatus == "empty"){
-    var allalpha = "abcdefghijklmnopqrstuvwxyz"; 
-    var x = Math.floor((Math.random() * 25) + 0);    
-    var openedwindow  = window.open("http://www.hobbyprojects.com/computer-terms-dictionary/computer-dictionary-"+allalpha[x]+"#chextlinkscan");
-    setTimeout(function(){ openedwindow.close(); }, 30000);
-}
-setTimeout(function(){
-    window.location.reload(1);
-}, trimtosec);
-</script>
 </html>
